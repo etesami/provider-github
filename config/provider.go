47fdd1d9d34950819d60642b244a5a39b3e316c3
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-04 23:58:02 +0000

    Run ./hack/prepare.sh

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/etesami/provider-github/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "github"
+	modulePath     = "github.com/etesami/provider-github"
 )
 
 //go:embed schema.json
