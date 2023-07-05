f933dd289807ec6dadd96813131005ebd034434b
Ehsan Etesami <eh.etesami@gmail.com>
2023-07-05 00:42:35 +0000

    Add branch and repository custom configs

--- config/provider.go
+++ config/provider.go
@@ -10,7 +10,8 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/etesami/provider-github/config/null"
+	"github.com/etesami/provider-github/config/branch"
+	"github.com/etesami/provider-github/config/repository"
 )
 
 const (
@@ -35,7 +36,8 @@ func GetProvider() *ujconfig.Provider {
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		repository.Configure,
+		branch.Configure,
 	} {
 		configure(pc)
 	}
